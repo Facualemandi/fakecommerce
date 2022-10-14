daac24c137eb70dd20e58da8b1b4a6a0ecbd6435
Facundo Alemandi <facuinstituto10@gmail.com>
2022-10-14 17:04:25 -0300

    Agregando lo que va por dentro de cada producto

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,6 +2,8 @@ import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
 import NoSearch from "../../images/nosearch.png";
+import BlueBuy from "../../images/buy-blue.svg";
+import Coin from "../../images/coin.svg";
 
 const Img = styled.img`
   width: 100%;
@@ -24,6 +26,20 @@ const Container = styled.section`
   }
 `;
 
+const DivHover = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  display: none;
+
+  &&:hover {
+    background-color: #00d9ffb2;
+    transition: 0.3s;
+    border-radius: 5px;
+  }
+`;
+
 const SectionProduct = styled.section`
   margin: 5px;
   font-family: "Roboto", sans-serif;
@@ -40,29 +56,12 @@ const SectionProduct = styled.section`
     transition: 0.6s;
     box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.15);
   }
-
-  p {
-    margin-top: 10px;
-    font-size: 18px;
-    padding: 5px;
-    font-weight: lighter;
+  &:hover ${DivHover} {
+    display: block;
   }
 
   @media (min-width: 780px) {
     cursor: pointer;
-  }
-`;
-
-const DivHover = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0px;
-
-  &&:hover {
-    background-color: #00d9ff73;
-    transition: 0.3s;
-    border-radius: 5px;
   }
 `;
 
@@ -86,7 +85,52 @@ const ImgNoSearch = styled.img`
   width: 280px;
 `;
 
+const ImgAdd = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
+const DivContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DivPoints = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  p{
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const Reedem = styled.p`
+font-size: 20px;
+border-radius: 30px;
+padding: 10px;
+background-color: rgba(0, 0, 0, 0.600);
+color: white;
+font-weight: lighter;
+margin-top: 15px;
+`
+
+const Name = styled.p`
+font-size: 22px;
+font-weight: lighter;
+padding: 10px;
+margin-top: 15px;
+`
+
 const Products = ({ searchProduct }) => {
+  console.log(searchProduct);
   return (
     <AnimatePresence>
       <Container>
@@ -100,8 +144,18 @@ const Products = ({ searchProduct }) => {
           >
             <SectionProduct key={product._id}>
               <Img alt="" src={product.img.hdUrl} />
-              <p>{product.name}</p>
-              <DivHover></DivHover>
+              <Name>{product.name}</Name>
+              <DivHover>
+                <ImgAdd alt="Buy " src={BlueBuy} />
+
+                <DivContainer>
+                  <DivPoints>
+                    <p>{product.cost}</p>
+                    <img alt="" src={Coin} />
+                  </DivPoints>
+                  <Reedem>Reedem Now</Reedem>
+                </DivContainer>
+              </DivHover>
             </SectionProduct>
           </motion.div>
         ))}
