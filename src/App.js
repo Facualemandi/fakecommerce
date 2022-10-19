920d53ed026f5173183507e5b17f2ac3b2cfe5df
Facundo Alemandi <facuinstituto10@gmail.com>
2022-10-19 16:40:25 -0300

    Cambiando el nombre a Aerofake

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home/Home";
 import { ContextProvider } from "./context/context";
-import  {ReactQueryDevtools}  from "@tanstack/react-query-devtools";
 
 function App() {
   const queryClient = new QueryClient();
@@ -15,7 +14,6 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </ContextProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </>
   );
